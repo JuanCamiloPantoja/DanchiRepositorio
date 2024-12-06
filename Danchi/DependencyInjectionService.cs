3da840ea4f9dbc958a5feb902088bbbef413ddd7
BrayanPulido06 <brayan06.pulido@gmail.com>
2024-12-06 15:03:33 -0500

    Commit de dependencias.

---
+++ Danchi/DependencyInjectionService.cs
@@ -0,0 +1,32 @@
+﻿using Danchi.Context;
+using Danchi.Repositories;
+using Danchi.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Danchi
+{
+    public static class DependencyInjectionService
+    {
+        public static IServiceCollection AddExternal(this IServiceCollection services, IConfiguration _configuration)
+        {
+            string connectionString = "";
+            connectionString = _configuration["ConnectionStrings:SQLConnectionStrings"];
+
+            services.AddDbContext<Danchi_Context>(options => options.UseSqlServer(connectionString));
+            services.AddScoped<IAdministradorRepository, AdministradorRepository>();
+            services.AddScoped<IAnuncioAcontecimientosRepository, AnuncioAcontecimientosRepository>();
+            services.AddScoped<IAutenticacionUsuarioRepository, AutenticacionUsuarioRepository>();
+            services.AddScoped<IChatInternoRepository, ChatInternoRepository>();
+            services.AddScoped<INotificacionEmergenciasRepository, NotificacionEmergenciasRepository>();
+            services.AddScoped<IResidenteRepository, ResidenteRepository>();
+            services.AddScoped<ISoporteTecnicoRepository, SoporteTecnicoRepository>();
+            services.AddScoped<ISugerenciasReporteErroresRepository, SugerenciasReporteErroresRepository>();
+
+
+
+
+
+            return services;
+        }
+    }
+}
