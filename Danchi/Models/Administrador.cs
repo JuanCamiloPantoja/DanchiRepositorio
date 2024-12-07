9bcc60c73ba6c399c96f4d3d16c43ec68b884134
Ash20 <Ash20@CAMILO>
2024-12-07 14:07:33 -0500

    Commit de tipos de datos en los modelos

--- Danchi/Models/Administrador.cs
+++ Danchi/Models/Administrador.cs
@@ -4,7 +4,7 @@
     {
         public int IdAdministrador { get; set; }
         public string Nombre { get; set; }
-        public int CelularAdministrador { get; set; }
+        public decimal CelularAdministrador { get; set; }
         public string CorreoElectronicoAd { get; set; }
     }
 }
--- Danchi/Models/Residente.cs
+++ Danchi/Models/Residente.cs
@@ -5,7 +5,7 @@
         public int IdResidente { get; set; }
         public string NumApartamento { get; set; }
         public string CorreoElectronico { get; set; }
-        public int CelularResidente { get; set; }
+        public decimal CelularResidente { get; set; }
         public string Nombre { get; set; }
     }
 }
