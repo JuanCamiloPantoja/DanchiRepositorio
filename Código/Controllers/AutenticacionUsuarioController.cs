174d8bb49a9bcd5bc83ab97f65f01efe61fb6e5f
JuanCamiloPantoja <juancamilopantojaojeda@gmail.com>
2024-12-13 15:30:11 -0500

    Add files via upload

---
+++ Código/Controllers/AutenticacionUsuarioController.cs
@@ -0,0 +1,94 @@
+﻿using Danchi.Models;
+using Danchi.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Danchi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticacionUsuarioController : ControllerBase
+    {
+        private readonly IAutenticacionUsuarioRepository _repository;
+
+        public AutenticacionUsuarioController(IAutenticacionUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("GetAutenticacionUsuario")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetAutenticacionUsuario()
+        {
+            var response = await _repository.GetAutenticacionUsuario();
+            return Ok(response);
+        }
+
+        [HttpPost("PostAutenticacionUsuario")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PostAutenticacionUsuario([FromBody] AutenticacionUsuario autenticacionUsuario)
+        {
+            try
+            {
+                var response = await _repository.PostAutenticacionUsuario(autenticacionUsuario);
+                if (response == true)
+                    return Ok("Insertado correctamente");
+                else
+                    return BadRequest(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("PutAutenticacionUsuario")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PutAutenticacionUsuario(int id, [FromBody] AutenticacionUsuario autenticacionUsuario)
+        {
+            if (id != autenticacionUsuario.IdAutenticacion)
+            {
+                return BadRequest("El ID de la autenticación no coincide con el proporcionado.");
+            }
+            try
+            {
+                var response = await _repository.PutAutenticacionUsuario(autenticacionUsuario);
+                if (response)
+                    return Ok("Actualizado correctamente");
+                else
+                    return NotFound("Usuario no encontrado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteAutenticacionUsuario/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteAutenticacionUsuario(int id)
+        {
+            try
+            {
+                var response = await _repository.DeleteAutenticacionUsuario(id);
+                if (response)
+                    return Ok("Eliminado correctamente");
+                else
+                    return NotFound("Usuario no encontrado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Código/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using Danchi.Context;
+using Danchi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Danchi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Danchi_Context _context;
+
+        public AuthController(IConfiguration configuration, Danchi_Context context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] Login login)
+        {
+            if (login == null || string.IsNullOrEmpty(login.Email) || string.IsNullOrEmpty(login.Password))
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            // Verificar si el usuario existe en la base de datos
+            var AutenticacionUsuario = _context.autenticacionUsuario
+                .FirstOrDefault(u => u.Usuario == login.Email);
+
+            if (AutenticacionUsuario == null || AutenticacionUsuario.Contraseña != login.Password)
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            // Generación del token JWT
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, AutenticacionUsuario.Usuario),  // Usamos el correo como nombre de usuario
+                    new Claim(ClaimTypes.Role, AutenticacionUsuario.TipoUsuario) // El tipo de usuario como rol
+                },
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signinCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new { Token = tokenString });
+        }
+    }
+
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
